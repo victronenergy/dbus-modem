79e7391b7cb8682d654cf0efa48952487f587259
Mans Rullgard <mans@mansr.com>
2018-07-04 15:28:37 +0100

    v0.2

--- dbus-modem.py
+++ dbus-modem.py
@@ -17,7 +17,7 @@ from settingsdevice import SettingsDevice
 import logging
 log = logging.getLogger()
 
-VERSION = '0.1'
+VERSION = '0.2'
 
 modem_settings = {
     'connect': ['/Settings/Modem/Connect', 1, 0, 1],
